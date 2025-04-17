91fce244e6e40063211b130caccd4046f090369a
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-17 23:42:49 +0200

    vyřešení 3. úkolu

---
+++ hra.js
@@ -0,0 +1,17 @@
+let currentPlayer = 'circle'
+
+document.querySelectorAll(".game__field").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (currentPlayer === "circle") {
+            document.querySelector(".game__player img").src = "img/cross.svg"
+            e.target.classList.add("game__field--circle")
+            e.target.disabled = true
+            currentPlayer = "cross"
+        } else {
+            document.querySelector(".game__player img").src = "img/circle.svg"
+            btn.classList.add("game__field--cross")
+            e.target.disabled = true
+            currentPlayer = "circle"
+        }
+    })
+})
