f9853439689eae77ce7ac07f5ab58fee4ef79457
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-04 23:09:47 +0200

    vyřešení 4. úkolu - přidána funkce findWinner, která podle kliknutí na tlačítko, které vyplní pole herniPole, určí vítěze hry

--- hra.js
+++ hra.js
@@ -1,4 +1,25 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4'
+
+const herniPole = Array(100)
+herniPole.fill("_")
 let currentPlayer = "circle"
+
+document.querySelectorAll(".game__field").forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        const player = currentPlayer === "circle" ? "o" : "x"
+        herniPole[i] = player
+        const winner = findWinner(herniPole)
+
+        setTimeout(() => {
+            if (winner === "o" || winner === "x") {
+                alert(`Vyhrál hráč se symbolem ${winner}.`)
+                window.location.reload()
+            } else if (winner === "tie"){
+                alert(`Hra skončila remízou.`)
+            }
+        }, 250)
+    })
+})
 
 document.querySelectorAll(".game__field").forEach((btn) => {
     btn.addEventListener("click", (e) => {
